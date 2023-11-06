34699251e293af3eb280be4bc3694018b7a9fdf4
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-06 21:18:23 +0200

    додала вивід

--- main.py
+++ main.py
@@ -54,3 +54,34 @@ account = BankAccount(1000)
 account.deposit(500)
 account.withdraw(200)
 print(account.get_balance())  # Результат: 1300
+
+#
+
+class CalendarApp:
+    def __int__(self, name, date, description):
+        self.__name = name
+        self.__data = date
+        self.__description = description
+
+    def get_name(self):
+        return self.__name
+
+    def get_date(self):
+        return self.__date
+
+    def get_description(self):
+        return self.__description
+
+    def set_date(self, new_date):
+        self.__date = new_date
+
+    def set_description(self, new_description):
+        self.__description = new_description
+
+event = CalendarApp(1000)
+event.date(500)
+event.name(200)
+event.description(200)
+print(event.get_name())
+print(event.get_name())
+print(event.get_description())
