284e65031038e025c38c984a876a5148f9aa134b
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-11 19:15:43 +0200

    додала код виводу інформації

--- main.py
+++ main.py
@@ -2,15 +2,40 @@
 # Створіть клас "Комп'ютер", який має зберігати інформацію про процесор, ОЗУ та відеокарту. Застосуйте
 # інкапсуляцію для захисту цих даних від змін.
 
-class Comp:
+class Computer:
     def __init__(self, processor, OZY, videocard):
-        self.processor = processor
-
-    def OZY(self, OZY):
+        self._processor = processor
         self._OZY = OZY
-
-    def videocarde(self, videocard):
         self._videocard = videocard
+
+    def get_processor(self):
+        return self._processor
+
+    def get_OZY(self):
+        return self._OZY
+
+    def set_OZY(self, value):
+        self._OZY = value
+
+    def get_videocard(self):
+        return self._videocard
+
+    def set_videocard(self, value):
+        self._videocard = value
+
+
+computer = Computer("Intel i7", "16GB", "NVIDIA GeForce GTX 1080")
+print(f"Процесор: {computer.get_processor()}")
+print(f"ОЗУ: {computer.get_OZY()}")
+print(f"Відеокарта: {computer.get_videocard()}")
+
+
+computer.set_OZY("32GB")
+computer.set_videocard("NVIDIA GeForce RTX 3070")
+
+print(f"Оновлені дані: {computer.get_processor()}, {computer.get_OZY()}, {computer.get_videocard()}")
+
+
 
 
 
