0a92037f3f435ef3252a3210a3fb5b94f11c768f
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-11 18:11:50 +0200

    додамо метод __str__

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Завдання 1
+#Створіть клас "Користувач" з атрибутами "ім'я", "вік" та "email". Застосуйте інкапсуляцію, щоб забезпечити, що ці
+#дані можна отримати лише через методи класу.
 
 #Рівень доступу приватний
 class User:
@@ -6,7 +8,6 @@ class User:
         self._name = name
         self.__age = age
         self.__email = email
-
 
     def get_name(self):
         return self._name
@@ -17,10 +18,11 @@ class User:
     def get_email(self):
         return self.__email
 
+    def __str__(self):
+        return f"User: {self._name}, Age: {self.__age}, Email: {self.__email}"
 
-user1 = User("Ivan", 19, "example@gmail. com")
+user1 = User("Ivan", 19, "example@gmail.com")
 print(user1.get_name())
 print(user1.get_age())
 print(user1.get_email())
 print(user1)
-user
