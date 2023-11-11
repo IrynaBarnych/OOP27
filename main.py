8d220f0e04e6ab90851464a01801e44db7d1f05b
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-11 19:04:54 +0200

    початкові дання

--- main.py
+++ main.py
@@ -2,7 +2,15 @@
 # Створіть клас "Комп'ютер", який має зберігати інформацію про процесор, ОЗУ та відеокарту. Застосуйте
 # інкапсуляцію для захисту цих даних від змін.
 
+class Comp:
+    def __init__(self, processor, OZY, videocard):
+        self.processor = processor
 
+    def OZY(self, OZY):
+        self._OZY = OZY
+
+    def videocarde(self, videocard):
+        self._videocard = videocard
 
 
 
